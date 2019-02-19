fc647a96093fd0e99ef4c3dfd99c0bd9ca805576
Hendrik Mölder <hendrik@molder.eu>
2019-02-19 15:22:42 +0000

    Fix downloads deletion path

--- new_services/service/main.py
+++ new_services/service/main.py
@@ -10,6 +10,7 @@ set port number
 '''
 host = '127.0.0.1'
 port = 8881
+downloads_dir =  os.path.join(os.path.abspath(os.path.dirname(__file__)), 'downloads')
 
 # Include the API functions
 from endpoints import weather_json2csv, file_downloader, union
@@ -18,12 +19,19 @@ from endpoints import weather_json2csv, file_downloader, union
 if __name__ == '__main__':
     run( host = host, port = port)
 
+# Source: https://www.tutorialspoint.com/How-to-ignore-hidden-files-using-os-listdir-in-Python
+def file_is_hidden(p):
+    if os.name== 'nt':
+        attribute = win32api.GetFileAttributes(p)
+        return attribute & (win32con.FILE_ATTRIBUTE_HIDDEN | win32con.FILE_ATTRIBUTE_SYSTEM)
+    else:
+        return p.startswith('.') #linux-osx
+
 def clear_downloads():
-    dir =  os.path.join(os.path.abspath('__file__'), 'downloads')
-    print "[Shutdown] Clearing downloads from %s." % (dir)
-    files = os.listdir(dir)
+    print "[Shutdown] Clearing downloads from %s." % (downloads_dir)
+    files = [file for file in os.listdir(downloads_dir) if not file_is_hidden(file)]
     for file in files:
-        os.remove(os.path.join(dir,file))
+        os.remove(os.path.join(downloads_dir, file))
     print "[Shutdown] Downloaded files deleted."
 
 atexit.register(clear_downloads)
